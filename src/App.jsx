aa6abb78de75406e395550a02925bc7c18c4f6bb
Rakshith <rakshith3040@gmail.com>
2024-11-29 10:04:35 -0700

    fixed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Navhead from './components/navhead'
+import Navhead from './components/Navhead'
 
 function App() {
   const [count, setCount] = useState(0)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        'blueColor':'#2a68ff',
-        'greyIsh':'#f1f4f8',
-        'cardShadow':'#f7f8f9',
-        'textColor':'#252b36',
-        'customBackground': '#DAE9EE',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
